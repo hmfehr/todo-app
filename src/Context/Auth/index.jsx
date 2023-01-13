6cc91ed5faae496b01bb2d0c296dd487e10164d6
Hunter Fehr <hunter.fehr3935@yahoo.com>
2023-01-12 17:58:01 -0800

    wiring auth to validate the token using axios

--- src/Context/Auth/index.jsx
+++ src/Context/Auth/index.jsx
@@ -1,29 +1,30 @@
 import React, {useState} from 'react';
 import jwtDecode from 'jwt-decode';
+import axios from 'axios';
 
 export const AuthContext = React.createContext();
 
-const testUsers = {
-  admin: {
-    username: 'admin',
-    password: 'ADMIN',
-    email: 'admin@fakeuser.com',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTBlZDFiMzNjZTQ5MDAxODlmMzhiNyIsImNhcGFiaWxpdGllcyI6WyJjcmVhdGUiLCJ1cGRhdGUiLCJyZWFkIiwiZGVsZXRlIl0sInR5cGUiOiJ1c2VyIiwiaWF0IjoxNjU4OTA3OTMxLCJleHAiOjE2NTg5MTE1MzF9.bqe-52if5K50rGn30P4fheuAa2qWuxse9tWiuH4cnUM',
-  },
-  editor: {
-    username: 'editor',
-    password: 'EDITOR',
-    email: 'editor@fakeuser.com',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTBlZjk5MzNjZTQ5MDAxODlmMzhiYSIsImNhcGFiaWxpdGllcyI6WyJjcmVhdGUiLCJ1cGRhdGUiLCJyZWFkIl0sInR5cGUiOiJ1c2VyIiwiaWF0IjoxNjU4OTA4NTY5LCJleHAiOjE2NTg5MTIxNjl9.073ppQCHbplYN9befn8JElcP4zgFX6TEe_ARUQZc0KU',
-  },
-  user: {
-    username: 'user',
-    password: 'USER',
-    email: 'user@fakeuser.com',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTBmMGZjMzNjZTQ5MDAxODlmMzhjMCIsImNhcGFiaWxpdGllcyI6WyJyZWFkIl0sInR5cGUiOiJ1c2VyIiwiaWF0IjoxNjU4OTA4OTI0LCJleHAiOjE2NTg5MTI1MjR9.t7c7k2LbaTxsdfYjx_WC3QiP4MycU8sZryVyXQqJQH',
-  }
+// const testUsers = {
+//   admin: {
+//     username: 'admin',
+//     password: 'ADMIN',
+//     email: 'admin@fakeuser.com',
+//     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTBlZDFiMzNjZTQ5MDAxODlmMzhiNyIsImNhcGFiaWxpdGllcyI6WyJjcmVhdGUiLCJ1cGRhdGUiLCJyZWFkIiwiZGVsZXRlIl0sInR5cGUiOiJ1c2VyIiwiaWF0IjoxNjU4OTA3OTMxLCJleHAiOjE2NTg5MTE1MzF9.bqe-52if5K50rGn30P4fheuAa2qWuxse9tWiuH4cnUM',
+//   },
+//   editor: {
+//     username: 'editor',
+//     password: 'EDITOR',
+//     email: 'editor@fakeuser.com',
+//     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTBlZjk5MzNjZTQ5MDAxODlmMzhiYSIsImNhcGFiaWxpdGllcyI6WyJjcmVhdGUiLCJ1cGRhdGUiLCJyZWFkIl0sInR5cGUiOiJ1c2VyIiwiaWF0IjoxNjU4OTA4NTY5LCJleHAiOjE2NTg5MTIxNjl9.073ppQCHbplYN9befn8JElcP4zgFX6TEe_ARUQZc0KU',
+//   },
+//   user: {
+//     username: 'user',
+//     password: 'USER',
+//     email: 'user@fakeuser.com',
+//     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTBmMGZjMzNjZTQ5MDAxODlmMzhjMCIsImNhcGFiaWxpdGllcyI6WyJyZWFkIl0sInR5cGUiOiJ1c2VyIiwiaWF0IjoxNjU4OTA4OTI0LCJleHAiOjE2NTg5MTI1MjR9.t7c7k2LbaTxsdfYjx_WC3QiP4MycU8sZryVyXQqJQH',
+//   }
   
-}
+// }
 
 const AuthProvider = ({children}) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -38,7 +39,7 @@ const AuthProvider = ({children}) => {
   const _validateToken = (token) =>{
     try {
       let validUser = jwtDecode(token);
-      console.log('validUser', validUser);
+      // console.log('validUser', validUser);
       if(validUser){
         setUser(validUser);
         setIsLoggedIn(true);
@@ -50,13 +51,23 @@ const AuthProvider = ({children}) => {
     }
   };
 
-  const login = (username, password) =>{
-    let authCrededentials = testUsers[username];
+  const login = async (username, password) =>{
+    // let authCrededentials = testUsers[username];
 
+    let config = {
+      baseURL: 'https://api-js401.herokuapp.com',
+      url:'/signin',
+      method:'post',
+      auth: {username, password}
+    }
+
+    let response = await axios(config);
+    console.log('response data', response.data)
+    const {token} = response.data;
     // validate we have the correct answer
-    if(authCrededentials && authCrededentials.password === password){
+    if(token){
       try{
-        _validateToken(authCrededentials.token);
+        _validateToken(token);
       } catch (e){
         setError(e);
         console.error(e);
