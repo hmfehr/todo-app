1cbc6121cb871299931b787aaa8ad8f82c93e9e5
Hunter Fehr <hunter.fehr3935@yahoo.com>
2023-02-17 15:57:23 -0800

    adding styling and worked on funtionallity of the status bar

--- src/Components/SettingsForm/index.jsx
+++ src/Components/SettingsForm/index.jsx
@@ -1,28 +1,37 @@
-import { IconSettings } from '@tabler/icons';
-import { createStyles, Switch, Grid, Card, Text, NumberInput, TextInput, Button } from '@mantine/core';
-import { useContext } from 'react';
-import { SettingsContext } from '../../Context/Settings';
-import { useState } from 'react';
+import { IconSettings } from "@tabler/icons";
+import {
+  createStyles,
+  Switch,
+  Grid,
+  Card,
+  Text,
+  NumberInput,
+  TextInput,
+  Button,
+} from "@mantine/core";
+import { useContext } from "react";
+import { SettingsContext } from "../../Context/Settings";
+import { useState } from "react";
+import { When } from "react-if";
 
 const useStyles = createStyles((theme) => ({
   h1: {
     backgroundColor: theme.colors.gray[8],
     color: theme.colors.gray[0],
-    width: '80%',
-    margin: 'auto',
+    width: "80%",
+    margin: "auto",
     fontSize: theme.spacing.lg,
     padding: theme.spacing.md,
     marginBottom: theme.spacing.md,
     marginTop: theme.spacing.md,
-
-  }
+  },
 }));
-
 
 const SettingsForm = () => {
   const [show, setShow] = useState(false);
 
-  const { showComplete,
+  const {
+    showComplete,
     setShowComplete,
     pageItems,
     setPageItems,
@@ -37,32 +46,37 @@ const SettingsForm = () => {
     e.preventDefault();
     setShow(true);
     saveLocally();
-  }
+  };
   return (
     <>
-      <h1 className={classes.h1}><IconSettings />Manage Settings</h1>
-      <Grid style={{ width: '80%', margin: 'auto' }}>
+      <h1 className={classes.h1}>
+        <IconSettings />
+        Manage Settings
+      </h1>
+      <Grid style={{ width: "80%", margin: "auto" }}>
         <Grid.Col xs={12} sm={6}>
           <Card withBorder p='sm'>
             <Card.Section>
               <Text>Updated Settings</Text>
               <form onSubmit={handleSubmit}>
                 <Switch
-                label='Show Completed ToDos' 
-                checked={showComplete} 
-                onChange={(event) => setShowComplete(event.currentTarget.checked)}
+                  label='Show Completed ToDos'
+                  checked={showComplete}
+                  onChange={(event) =>
+                    setShowComplete(event.currentTarget.checked)
+                  }
                 />
-                <NumberInput 
-                md='sm'
-                value={pageItems}
-                label='your age'
-                onChange={(value)=> setPageItems(value)}
+                <NumberInput
+                  md='sm'
+                  value={pageItems}
+                  label='your age'
+                  onChange={(value) => setPageItems(value)}
                 />
-                <TextInput 
-                mb='sm'
-                placeholder={sort}
-                onChange={(e)=> setSort(e.target.value)}
-                label='Sort KeyWord'
+                <TextInput
+                  mb='sm'
+                  placeholder={sort}
+                  onChange={(e) => setSort(e.target.value)}
+                  label='Sort KeyWord'
                 />
                 <Button type='submit'>Show New Settings</Button>
               </form>
@@ -70,44 +84,23 @@ const SettingsForm = () => {
           </Card>
         </Grid.Col>
         <Grid.Col xs={12} sm={6}>
-
+          <When condition={show}>
+            <Card withBorder p='sm'>
+              <Card.Section>
+                <Text m="xl" fontSize="xl" weight="bold"></Text>
+              </Card.Section>
+              <Text m='sm'>{showComplete ? 'Show' : 'Hide'} Completed Todos</Text>
+              <Text m='sm'>Items Per Page {pageItems}</Text>
+              <Text m='sm'>Sort Keyword: {sort}</Text>
+            </Card>
+          </When>
         </Grid.Col>
       </Grid>
-
     </>
-  )
+  );
 };
 
 export default SettingsForm;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // export const SettingsContext = React.createContext();
 
