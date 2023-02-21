57a6b9314b36060973b2f94d9eaaa0574b385d19
Hunter Fehr <hunter.fehr3935@yahoo.com>
2023-02-20 23:31:20 -0800

    worked on toggle functionallity

--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -1,4 +1,4 @@
-import { Badge, Card, Pagination, Text } from "@mantine/core";
+import { Badge, Card, CloseButton, Group, Pagination, Text } from "@mantine/core";
 import { useContext, useState } from "react";
 import { If, Then, Else } from "react-if";
 import { AuthContext } from "../../Context/Auth";
@@ -6,11 +6,11 @@ import { SettingsContext } from "../../Context/Settings";
 
 const List = ({ list, toggleComplete }) => {
   const { can, isLoggedIn } = useContext(AuthContext);
-  const { showCompleted, pageItems } = useContext(SettingsContext);
+  const { showComplete, pageItems } = useContext(SettingsContext);
   const [page, setPage] = useState(1);
 
   // pagination logic
-  const listToRender = showCompleted
+  const listToRender = showComplete
     ? list
     : list.filter((item) => !item.complete);
   const listStart = pageItems * (page - 1);
@@ -21,41 +21,43 @@ const List = ({ list, toggleComplete }) => {
   return (
     <>
       {displayList.map((item) => (
-        <Card key={item.id} withBorder shadow='md'>
+        <Card key={item.id} withBorder shadow='md' mb="sm">
           <Card.Section withBorder>
-            <If condition={isLoggedIn && can("update")}>
-              <Then>
-                <Badge 
-                color={item.complete ? "red" : "green"} 
-                variant='filled'
-                onClick={() => toggleComplete(item.id)}
-                >
-                  {item.complete ? "Complete" : "Pending"}
-                </Badge>
-              </Then>
-              <Else>
-                <Badge 
-                color={item.complete ? "red" : "green"} 
-                variant='filled'>
-                  {item.complete ? "Complete" : "Pending"}
-                </Badge>
-              </Else>
-            </If>
-
-            <Text>{item.assignee}</Text>
+            <Group position='apart'>
+            <Group>
+              <If condition={isLoggedIn && can("update")}>
+                <Then>
+                  <Badge
+                    color={item.complete ? "red" : "green"}
+                    variant='filled'
+                    onClick={() => toggleComplete(item.id)}
+                    m="3px"
+                  >
+                    {item.complete ? "Complete" : "Pending"}
+                  </Badge>
+                </Then>
+                <Else>
+                  <Badge
+                    color={item.complete ? "red" : "green"}
+                    variant='filled'
+                    m="3px"
+                  >
+                    {item.complete ? "Complete" : "Pending"}
+                  </Badge>
+                </Else>
+              </If>
+              <Text>{item.assignee}</Text>
+            </Group>
+              <CloseButton 
+                title='Close Todo Item'
+                onClick={() => delete}
+              />
+            </Group>
           </Card.Section>
+          <Text mt='sm'>{item.text}</Text>
+          <Text align='right'>Difficulty: {item.difficulty}</Text>
 
-          <p>{item.id}</p>
-          <p>
-            <small>Assigned to: {item.assignee}</small>
-          </p>
-          <p>
-            <small>Difficulty: {item.difficulty}</small>
-          </p>
-          <div >
-            Complete: {item.complete.toString()}
-          </div>
-          <hr />
+
         </Card>
       ))}
 
